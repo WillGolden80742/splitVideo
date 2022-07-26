8aef4d6e97878b9817a446f54e10bba60fdf8d42
WillGolden80742 <william80742@gmail.com>
2022-07-26 20:11:35 -0300

    aperfeiçoando o algoritmo

--- splitVideo.py
+++ splitVideo.py
@@ -3,7 +3,7 @@ from time import sleep
 from datetime import datetime
 import os
 
-full_video = str(input("Insert file path of video :\n"))
+full_video = str(input("\nInsert file path of video :\n"))
 full_video = full_video.replace("\'","")[:-1]
 
 arrPath = full_video.split("/")
@@ -17,7 +17,7 @@ total_duration = VideoFileClip(full_video).duration
 single_duration=total_duration+1
 while (single_duration>total_duration):
     try:
-        single_duration = int(input("Enter the limit of seconds that each video must have :\n"))
+        single_duration = int(input("\nEnter the limit of seconds that each video must have :\n"))
         if single_duration > total_duration or total_duration == 0:
             os.system('clear')
             print ("The seconds must be at least less than the total seconds of the video or different from 0")
@@ -34,10 +34,10 @@ part=0
 
 while finalSubClip <= total_duration:
     part+=1
+    current_video = folderName+"_part"+str(part)+".mp4"
     clip = VideoFileClip(full_video).subclip(initialSubClip,finalSubClip)
-    print ("i : "+str(initialSubClip)+", f: "+str(finalSubClip))
+    print ("\n"+current_video+" = { \n  initial second : "+str(initialSubClip)+",\n  final second : "+str(finalSubClip)+"\n}\n")
     initialSubClip=finalSubClip
     finalSubClip+=single_duration
-    current_video = folderName+"_part"+str(part)+".mp4"
     clip.to_videofile(save_path+"/"+current_video, codec="libx264", temp_audiofile='temp-audio.m4a', remove_temp=True, audio_codec='aac')
     print("-----------------###-----------------")
